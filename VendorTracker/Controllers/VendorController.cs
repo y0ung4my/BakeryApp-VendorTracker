319d0efb617c88bac893c497a5360a7a5280ddd4
Amy Young <youngamy1223@gmail.com>
2022-02-26 22:15:48 -0700

    add link to vendors page

---
+++ VendorTracker/Controllers/VendorController.cs
@@ -0,0 +1,59 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using VendorTracker.Models;
+
+namespace VendorTracker.Controllers
+{
+  public class VendorController : Controller
+  {
+
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
+
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName, string vendorDescription)
+    {
+      Vendor newVendor = new Vendor(vendorName, vendorDescription);
+      return RedirectToAction("Index");
+    }
+
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> vendorOrders = selectedVendor.Orders; 
+      model.Add("vendor", selectedVendor);
+      model.Add("orders", vendorOrders); 
+      return View(model);
+    }
+
+
+    // This one creates new Items within a given Category, not new Categories:
+
+    // [HttpPost("/vendors/{vendorId}/orders")]
+    // public ActionResult Create(int vendorId, string orderTitle, string orderDescription, string orderPrice, string orderDate)
+    // {
+    //   Dictionary<string, object> model = new Dictionary<string, object>();
+    //   Vendor foundVendor = Vendor.Find(vendorId);
+    //   Order newOrder = new Order(orderTitle, orderDescription, orderPrice, orderDate);
+    //   foundVendor.AddOrder(newOrder);
+    //   List<Order> vendorOrders = foundVendor.Orders;
+    //   model.Add("orders", vendorOrders);
+    //   model.Add("vendors", foundVendor);
+    //   return View("Show", model);
+    // }
+
+  }
+}
