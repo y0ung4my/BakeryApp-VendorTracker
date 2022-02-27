8895763179aaab09129809863266abaecdc2abdf
Amy Young <youngamy1223@gmail.com>
2022-02-26 19:32:21 -0700

    add GetAll() for Orders list and test

--- VendorTracker.Tests/ModelTests/Order.Tests.cs
+++ VendorTracker.Tests/ModelTests/Order.Tests.cs
@@ -1,12 +1,17 @@
 using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using VendorTracker.Models;
+using System.Collections.Generic;
 
 namespace VendorTracker.Tests
 {
   [TestClass]
-  public class OrderTests
+  public class OrderTests : IDisposable
   {
+    public void Dispose()
+    {
+      Order.ClearAll();
+    }
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfItem()
     {
@@ -69,6 +74,33 @@ namespace VendorTracker.Tests
       Assert.AreEqual(updatedDate, updatedDate); 
     }
 
-    
+    [TestMethod]
+    public void GetAll_ReturnsOrders_OrderList()
+    {
+      //Arrange
+      string title1 = "Recurring croissant order for Suzie's Cafe";
+      string description1 = "50 croissants, daily";
+      string price1 = "$75/day";
+      string date1 = "2/26/2022";
+      string title2 = "Recurring bread order for Marty's Grocery";
+      string description2 = "50 loaves of bread delivered every Sunday and Thursday";
+      string price2 = "$300/week";
+      string date2 = "2/26/2022";
+      Order newOrder1 = new Order(title1, description1, price1, date1);
+      Order newOrder2 = new Order(title2, description2, price2, date2);
+      List<Order> newList = new List<Order> { newOrder1, newOrder2 };
+
+      //Act
+      List<Order> result = Order.GetAll();
+
+      foreach (Order thisOrder in result)
+      {
+        Console.WriteLine("Output from second GetAll test: " + thisOrder.Title); 
+      }
+
+      //Assert
+      CollectionAssert.AreEqual(newList, result);
+    }    
+
   }
 }
--- VendorTracker/Models/Order.cs
+++ VendorTracker/Models/Order.cs
@@ -8,6 +8,7 @@ namespace VendorTracker.Models
     public string Description { get; set; }
     public string Price { get; set; }
     public string Date { get; set; }
+    private static List<Order> _instances = new List<Order> {};
 
     public Order (string title, string description, string price, string date)
     {
@@ -15,6 +16,17 @@ namespace VendorTracker.Models
       Description = description;
       Price = price;
       Date = date;
+      _instances.Add(this);
+    }
+
+    public static List<Order> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
     }
 
   }
